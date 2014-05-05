32e7f99d87ed0db716897bd043b634b1a05f9097
Nate Lundie <ngl142@gmail.com>
2014-05-05 16:33:03 -0400

    initial stuff
    can currently follow you, and make a trail behind you

---
+++ GMapsTest/ViewController.m
@@ -0,0 +1,171 @@
+//
+//  ViewController.m
+//  GMapsTest
+//
+//  Created by Chris Sutton on 5/2/14.
+//  Copyright (c) 2014 Chris Sutton. All rights reserved.
+//
+
+#import "ViewController.h"
+#import <GoogleMaps/GoogleMaps.h>
+#import "CoreLocation/CoreLocation.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController {
+    GMSMapView *mapView_;
+}
+@synthesize elevationStuff;
+@synthesize theStringiestStringThatHasEverStringed;
+@synthesize locationManager;
+@synthesize altArray;
+@synthesize longArray;
+@synthesize latArray;
+@synthesize path;
+
+- (void)viewDidLoad {
+    [self loadSoups];
+    // Create a GMSCameraPosition that tells the map to display the
+    // coordinate -33.86,151.20 at zoom level 6.
+    locationManager = [[CLLocationManager alloc] init];
+    locationManager.desiredAccuracy = kCLLocationAccuracyBest;
+    locationManager.delegate = self;
+    [locationManager startUpdatingLocation];
+    GMSCameraPosition *camera = [GMSCameraPosition cameraWithLatitude:-33.85
+                                                            longitude:151.20
+                                                                 zoom:15];
+    mapView_ = [GMSMapView mapWithFrame:CGRectZero camera:camera];
+    mapView_.myLocationEnabled = YES;
+    mapView_.settings.myLocationButton = YES;
+    self.view = mapView_;
+    
+    // Creates a marker in the center of the map.
+    /*GMSMarker *marker = [[GMSMarker alloc] init];
+    marker.position = CLLocationCoordinate2DMake(-33.86, 151.20);
+    marker.title = @"Elevation here";
+    marker.snippet = theStringiestStringThatHasEverStringed;*/
+    //marker.map = mapView_;
+    //GMSMutablePath *path = [GMSMutablePath path];
+    path = [[GMSMutablePath alloc] init];
+    /*[path addCoordinate:CLLocationCoordinate2DMake(-33.85, 151.20)];
+    [path addCoordinate:CLLocationCoordinate2DMake(-33.70, 151.40)];
+    [path addCoordinate:CLLocationCoordinate2DMake(-33.73, 151.41)];
+    GMSPolyline *polyline = [GMSPolyline polylineWithPath:path];
+    polyline.map = mapView_;*/
+    //GMSCameraPosition *camera = [GMSCameraPosition cameraWithLatitude:-33.86
+      //                                                      longitude:151.20
+        //                                                         zoom:6];
+    altArray = [[NSMutableArray alloc] init];
+    latArray = [[NSMutableArray alloc] init];
+    longArray = [[NSMutableArray alloc] init];
+
+    
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+- (void) loadSoups
+{
+    elevationStuff = [[NSMutableArray alloc] init];
+    
+    NSURL *url = [[NSURL alloc] initWithString:[NSString stringWithFormat:@"https://maps.googleapis.com/maps/api/elevation/json?locations=39.7391536,-104.9847034&sensor=false&key=AIzaSyAlDakcBveHB6pyPqqkU-6RbHO2pGFiT2g"]];
+    
+    NSURLRequest *urlRequest = [NSURLRequest requestWithURL:url
+                                                cachePolicy:NSURLRequestReloadIgnoringCacheData
+                                            timeoutInterval:30];
+    
+    NSData *urlData;
+    NSURLResponse *response;
+    NSError *error;
+    
+    urlData = [NSURLConnection sendSynchronousRequest:urlRequest
+                                    returningResponse:&response
+                                                error:&error];
+    // NSLog(@"%@", urlData);
+    
+    NSDictionary *jsonDic = [NSJSONSerialization
+                             JSONObjectWithData:urlData
+                             options:0
+                             error:&error];
+    
+    // NSLog(@"%@", jsonDic);
+    NSDictionary *elevationDic = [jsonDic objectForKey:@"results"];
+    
+    //NSLog(@"Sides length is: %x", (unsigned int)[flikLiveDic count]);
+    //NSLog(@"Description: %@", [flikLiveDic description]);
+    
+    for(NSDictionary *subMeals in elevationDic){
+        NSString *holdString = [NSString stringWithFormat:@"%@", [subMeals objectForKey:@"elevation"]];
+        //NSLog(@"MealName: %@", holdString);
+        [elevationStuff addObject:holdString];
+    }
+    NSLog(@"lunchSoups: %@", elevationStuff);
+    theStringiestStringThatHasEverStringed = elevationStuff.description;
+    
+}
+
+#pragma mark -
+#pragma mark CLLocationManagerDelegate
+
+-(void)locationManager:(CLLocationManager *)manager
+   didUpdateToLocation:(CLLocation *)newLocation
+          fromLocation:(CLLocation *)oldLocation
+{
+    NSString *currentLatitude = [[NSString alloc]
+                                 initWithFormat:@"%+.6f",
+                                 newLocation.coordinate.latitude];
+    //latitude.text = currentLatitude;
+    
+    NSString *currentLongitude = [[NSString alloc]
+                                  initWithFormat:@"%+.6f",
+                                  newLocation.coordinate.longitude];
+    //longitude.text = currentLongitude;
+    
+    NSString *currentHorizontalAccuracy =
+    [[NSString alloc]
+     initWithFormat:@"%+.6f",
+     newLocation.horizontalAccuracy];
+    //horizontalAccuracy.text = currentHorizontalAccuracy;
+    
+    NSString *currentAltitude = [[NSString alloc]
+                                 initWithFormat:@"%+.6f",
+                                 newLocation.altitude];
+    //altitude.text = currentAltitude;
+    //altArray = [[NSMutableArray alloc] init];
+    //[altArray addObject:currentAltitude];
+    //NSLog(@"altArray: %@", altArray);
+    
+    NSString *currentVerticalAccuracy =
+    [[NSString alloc]
+     initWithFormat:@"%+.6f",
+     newLocation.verticalAccuracy];
+    //verticalAccuracy.text = currentVerticalAccuracy;
+    
+    //if (startLocation == nil)
+      //  startLocation = newLocation;
+    
+    //CLLocationDistance distanceBetween = [newLocation
+      //                                    distanceFromLocation:startLocation];
+    
+    /*NSString *tripString = [[NSString alloc]
+                            initWithFormat:@"%f",
+                            distanceBetween];*/
+    //distance.text = tripString;
+    [path addCoordinate:CLLocationCoordinate2DMake(newLocation.coordinate.latitude, newLocation.coordinate.longitude)];
+    [latArray addObject:currentLatitude];
+    NSLog(@"latArray: %@", latArray);
+    [longArray addObject:currentLongitude];
+    NSLog(@"longArray: %@", longArray);
+    [altArray addObject:currentAltitude];
+    NSLog(@"altArray: %@", altArray);
+    GMSPolyline *polyline = [GMSPolyline polylineWithPath:path];
+    polyline.map = mapView_;
+    
+}
+
+@end
